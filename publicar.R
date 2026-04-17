5bcc688665a6eb415b4555cc2e036962e48aaab1
vitorioaflalospparcerias <vitorio.aflalo@spparcerias.com.br>
2026-04-17 15:31:13 -0300

    Replace account info with placeholders in publicar.R
    Updated account information placeholders for deployment.

--- publicar.R
+++ publicar.R
@@ -1,17 +1,13 @@
 library(rsconnect)
-#rsconnect::setAccountInfo(name = 'saopaulo-parcerias',
-#                          token='56EE1707084C7F0979772937C8F07318',
-#                          secret='BM316BYtkhoNLClscYbassBoJAbUFLICD1LaPX4E')
 
-
-rsconnect::setAccountInfo(name='vitorioaflalo',
-                          token='7A27EAC28E7D90E00CDBB049BEB5E484',
-                          secret='lnlbEWUAoCwUvSLaJ2Wmmce8mjC2POlbTLufd4YO')
+rsconnect::setAccountInfo(name='SEU NOME',
+                          token='SEU TOKEN',
+                          secret='SEU SEGREDO')
 
 rsconnect::deployApp(
   appDir = getwd(),
-  appName = "parcerias-mapa-sp", 
-  account = "vitorioaflalo",
+  appName = "SEU APP NAME", 
+  account = "SUA CONTA",
   forceUpdate = TRUE
 )
 
