0fdad7dca259951a69dc58aeabc8146b2a82de86
Vitório Aflalo <vitorio.aflalo@spparcerias.local>
2026-04-27 10:41:40 -0300

    commit final

--- publicar.R
+++ publicar.R
@@ -1,17 +1,17 @@
 library(rsconnect)
-#rsconnect::setAccountInfo(name = 'saopaulo-parcerias',
-#                          token='56EE1707084C7F0979772937C8F07318',
-#                          secret='BM316BYtkhoNLClscYbassBoJAbUFLICD1LaPX4E')
+rsconnect::setAccountInfo(name='saopaulo-parcerias',
+                          token='5516061E976CA6462D88359A35243FB9',
+                          secret='45kkAUSpXYPFiykuFZ92erBH+Rpm7ZkPkMtwdvQU')
 
 
-rsconnect::setAccountInfo(name='vitorioaflalo',
-                          token='7A27EAC28E7D90E00CDBB049BEB5E484',
-                          secret='lnlbEWUAoCwUvSLaJ2Wmmce8mjC2POlbTLufd4YO')
+#rsconnect::setAccountInfo(name='vitorioaflalo',
+#                          token='7A27EAC28E7D90E00CDBB049BEB5E484',
+#                          secret='lnlbEWUAoCwUvSLaJ2Wmmce8mjC2POlbTLufd4YO')
 
 rsconnect::deployApp(
   appDir = getwd(),
   appName = "parcerias-mapa-sp", 
-  account = "vitorioaflalo",
+  account = "saopaulo-parcerias",
   forceUpdate = TRUE
 )
 
