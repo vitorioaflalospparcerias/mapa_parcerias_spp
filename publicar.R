3a5ebccbda144e06fb35c6bcab5994a1ed69bfae
vitorioaflalospparcerias <vitorio.aflalo@spparcerias.com.br>
2026-04-27 10:48:19 -0300

    Modify rsconnect account info with placeholders
    Updated account information for rsconnect with placeholders.

--- publicar.R
+++ publicar.R
@@ -1,12 +1,9 @@
 library(rsconnect)
-rsconnect::setAccountInfo(name='saopaulo-parcerias',
-                          token='5516061E976CA6462D88359A35243FB9',
-                          secret='45kkAUSpXYPFiykuFZ92erBH+Rpm7ZkPkMtwdvQU')
+rsconnect::setAccountInfo(name='SEU NOME',
+                          token='SEU TOKEN',
+                          secret='SEU SEGREDO')
 
 
-#rsconnect::setAccountInfo(name='vitorioaflalo',
-#                          token='7A27EAC28E7D90E00CDBB049BEB5E484',
-#                          secret='lnlbEWUAoCwUvSLaJ2Wmmce8mjC2POlbTLufd4YO')
 
 rsconnect::deployApp(
   appDir = getwd(),
